54f4f61aa6b36ade5b15dc368ecbbbd9f1950b48
Laert Huti <101653303+laerttt@users.noreply.github.com>
2024-10-31 16:41:02 +0100

    Sign

---
+++ Sign Of The Product Of An Array/main.py
@@ -0,0 +1,22 @@
+from typing import List
+def arraySign(nums: List[int]) -> int:
+    count=0
+    for i in range(len(nums)):
+        if nums[i]<0:
+            count+=1
+    if count%2==0 and 0 not in nums:
+        return 1
+    elif 0 in nums:
+        return 0 
+    return -1        
+
+example1 = [-1,-2,-3,-4,3,2,1]       
+print(arraySign(nums=example1))
+# Expected output => 1
+example2 = [1,5,0,2,-3]       
+print(arraySign(nums=example2))
+# Expected output => 0
+example3 = [-1,1,-1,1,-1]     
+print(arraySign(nums=example3))
+# Expected output => -1
+        
